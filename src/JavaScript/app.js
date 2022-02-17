eb41ec7ba61a02dda083f67a4fef165f90460411
ken.baum <ilpadre1953@gmail.com>
2022-02-16 19:43:55 -0500

    Did it all

--- src/JavaScript/app.js
+++ src/JavaScript/app.js
@@ -1,8 +1,29 @@
-// Implement your exercise in this file.  If you need to implement additional functions,
-// remember to export them as well, if you need to access them in your unit test.
-function doAllTheThings() {
-    console.log("Hi there");
-    return true;
+
+const timeConversion = (stdTime) => {
+    if (stdTime === '') {
+        return -1;
+    }
+    let militaryTime = '';
+    let amPm = stdTime.substring(stdTime.length-2);
+    let hour = stdTime.substring(0, 2);
+    let intHour = parseInt(hour);
+    let minSec = stdTime.substring(2, 8);
+    let zeroHour = '00';
+    if (amPm == 'AM') {
+        if (hour == '12') {
+            militaryTime = zeroHour + minSec;
+        } else {
+            militaryTime = hour + minSec;
+        }  
+
+    } else {
+        let milHour = intHour%12 + 12;
+        let milHourStr = '';
+         militaryTime = milHour.toString() + minSec;
+
+    }
+    return militaryTime;
+
 }
 
-module.exports = { doAllTheThings };
+module.exports = { timeConversion };
--- src/JavaScript/app.test.js
+++ src/JavaScript/app.test.js
@@ -1,6 +1,48 @@
-const { doAllTheThings } = require('./app');
+const { timeConversion } = require('./app');
 
 // This is a Jest unit test - see https://jestjs.io/docs/en/getting-started for more information
-test('doAllTheThings should be true', () => {
-    expect(doAllTheThings()).toBeTruthy();
-});
+// test('doAllTheThings should be true', () => {
+//     expect(doAllTheThings()).toBeTruthy();
+// });
+
+// empty input
+test('nimno forgot to put the date in', () => {
+    expect(timeConversion('')).toBe(-1);
+})
+
+// 12:00:00AM input
+test('12:00:00AM is 00:00:00', () => {
+    expect(timeConversion('12:00:00AM')).toBe('00:00:00');
+})
+
+// 01:01:00AM input
+test('01:01:00AM is 01:01:00', () => {
+    expect(timeConversion('01:01:00AM')).toBe('01:01:00');
+})
+
+// 11:59:59AM input
+test('11:59:59AM is 11:59:59', () => {
+    expect(timeConversion('11:59:59AM')).toBe('11:59:59');
+})
+
+// 08:30:02AM input
+test('08:30:02AM is 08:30:02', () => {
+    expect(timeConversion('08:30:02AM')).toBe('08:30:02');
+})
+
+test('12:00:00PM is 12:00:00', () => {
+    expect(timeConversion('12:00:00PM')).toBe('12:00:00');
+})
+
+
+test('01:01:00PM is 13:01:00', () => {
+    expect(timeConversion('01:01:00PM')).toBe('13:01:00');
+})
+
+test('11:59:59PM is 23:59:59', () => {
+    expect(timeConversion('11:59:59PM')).toBe('23:59:59');
+})
+
+test('08:30:02PM is 20:30:02', () => {
+    expect(timeConversion('08:30:02PM')).toBe('20:30:02');
+})
